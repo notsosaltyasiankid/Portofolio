24e8e41630979f5bddcdf924dd97b3e8512bf163
Alex Bons <alexwillembons@gmail.com>
2025-10-14 19:54:21 +0200

    Other project window demo

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.querySelector(".projects-btn").addEventListener("click", function() {
+
+const button = document.getElementById("ProjectsButton");
+const nav = document.getElementById("ProjectsMenu");
+const body = document.getElementById("Body")
+
+button.onclick = function(){
+    nav.scrollIntoView({ behavior: "instant" })
+    nav.style.visibility = "visible";
+    body.style.overflow = "hidden";
+    body.style.visibility = "hidden";
+}
+
+document.querySelector(".projects-btn").addEventListener("click", function() 
+{
     document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
 });
-
