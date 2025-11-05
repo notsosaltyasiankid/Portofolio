a43f5dacdc81666a20fb66689ebdfda8c577e876
Alex Bons <alexwillembons@gmail.com>
2025-11-05 22:29:50 +0100

    More projects button added and some minor spelling fixes

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 const buttonProjects = document.getElementById("ProjectsButton");
-const opacityButton = document.getElementById("Buttons")
+const backButton = document.getElementById("Buttons")
 const nav = document.getElementById("ProjectsMenu");
 const body = document.getElementById("Body")
 
 buttonProjects.onclick = function(){
-    nav.scrollIntoView({ behavior: "instant" })
+    nav.scrollIntoView({ behavior: "smooth" })
     nav.style.visibility = "visible";
     nav.style.opacity = 1;
     body.style.overflow = "hidden";
     body.style.visibility = "hidden";
+}
+
+backButton.onclick = function(){
+    body.style.overflow = "visible";
+    body.style.visibility = "visible";
+    body.style.opacity = 1;
+    nav.style.opacity = 0;
+    nav.style.visibility = "hidden";
 }
