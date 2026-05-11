4058609b33b43a8a138a7185bf0f9d91acc3813b
Alex Bons <alexwillembons@gmail.com>
2026-05-11 15:11:14 +0200

    layout change

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 const buttonProjects = document.getElementById("ProjectsButton");
-const backButton = document.getElementById("Buttons")
+const buttonProjects2 = document.getElementById("ProjectsButton2");
+const aboutButton = document.getElementById("AboutButton");
+const backButton = document.getElementById("Buttons");
 const nav = document.getElementById("ProjectsMenu");
-const body = document.getElementById("Body")
+const body = document.getElementById("Body");
+const Project = document.getElementById("projects");
+const About = document.getElementById("AboutMe")
 
 buttonProjects.onclick = function(){
     nav.scrollIntoView({ behavior: "smooth" })
@@ -18,4 +22,12 @@ backButton.onclick = function(){
     body.style.opacity = 1;
     nav.style.opacity = 0;
     nav.style.visibility = "hidden";
+}
+
+buttonProjects2.onclick = function(){
+    Project.scrollIntoView({ behavior: "smooth"})
+}
+
+aboutButton.onclick = function(){
+    About.scrollIntoView({ behavior: "smooth"})
 }
